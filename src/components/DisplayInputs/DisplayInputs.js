37576e65f09ebc5bbf9ace59240e9fa7e057b32e
mzoltan778 <mzoltan778@gmail.com>
2021-02-15 07:07:29 +0100

    save

--- src/components/DisplayInputs/DisplayInputs.js
+++ src/components/DisplayInputs/DisplayInputs.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import './DisplayInputs.css';
 
 export default function DisplayInputs(props) {
 
@@ -10,13 +11,15 @@ export default function DisplayInputs(props) {
         for (let index = 0; index < inputs.length; index++) {
             if (inputs[index].type)
                 if (inputs[index].type !== "textarea")
-                    ret.push(<div key={index}>
-                        <label>{inputs[index].label}</label>
-                        <input type={inputs[index].type} />
+                    ret.push(<div key={index} className="formField">
+                        <label>{inputs[index].label} </label>
+                        <input type={inputs[index].type} 
+                            //    value={inputs[index].type === "date" ? new Date().toISOString().slice(0, 10) : null} 
+                               />
                     </div>);
                 else
-                    ret.push(<div key={index}>
-                        <label>{inputs[index].label}</label>
+                    ret.push(<div key={index} className="formField">
+                        <label>{inputs[index].label} </label>
                         <textarea />
                     </div>);
             else 
@@ -37,7 +40,7 @@ export default function DisplayInputs(props) {
         content = [...content, ...renderInputs(props.inputs)];
 
     return (
-        <div>
+        <div className="displayInputs">
             <h2>{props.header}</h2>
             <h3>{props.header2}</h3>
             {
